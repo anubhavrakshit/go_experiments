9c24f1e48f6b6bae435e520ce1bef7ec6f75312c
Anubhav Rakshit <anubhavrocks@gmail.com>
2019-11-24 11:19:53 +0530

    Add math package

--- exp1/main.go
+++ exp1/main.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println("Whats this experiment?")
 	name, surname := "Anubhav", "Rakshit"
-	fmt.Println(name, surname)
+	age := 37.3
+	fmt.Println(name, surname, math.Floor(age))
 	someFloat := 3.14
 	fmt.Printf("%T %T\n", name, surname)
 	fmt.Printf("%T\n", someFloat)
