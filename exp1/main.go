cfa0a104a280a012f0c71fa5c8e844f2dbf5c9a0
Anubhav Rakshit <anubhavrocks@gmail.com>
2019-11-24 11:42:16 +0530

    Simple Array

--- exp1/main.go
+++ exp1/main.go
@@ -12,6 +12,11 @@ func main() {
 	fmt.Println("Whats this experiment?")
 	name, surname := "Anubhav", "Rakshit"
 	age := 37.3
+	// var bookArr [2]string
+	// bookArr[0] = "Book1"
+	// bookArr[1] = "Book2"
+	bookArr := [4]string{"Book 1", "Book 2"}
+	fmt.Println(bookArr[0], bookArr[1])
 	fmt.Println(name, surname, math.Floor(age))
 	someFloat := 3.14
 	fmt.Printf("%T %T\n", name, surname)
